609c193a17e6e9673b9c4d8edffdbed7db85eff8
shanaya <shanayasoans31@gmail.com>
2021-02-11 05:27:00 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ Author: Shanaya Soans 
+ Date: 1/29/20
+ Comments: This C# console application code demonstartes the use of Methods
+*/
+
+using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name: ");
+            String name = Console.ReadLine();
+            PrintName(name);
+        }
+        static void PrintName(string name)
+        {
+            Console.WriteLine("Hello " + name + "!");
+            Console.ReadLine();
+        }
+    }
+}
